8a0fd508a910e96545e3913046ce847f1026d904
ShadowDragonP <ryota.sakurai@hotmail.com>
2021-05-10 11:24:52 -0400

    ver 1.0

---
+++ ResourcePackUpdater/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace ResourcePackUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderPath = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.packFormat = new System.Windows.Forms.TextBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.logText = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.formatCheck = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Resource Pack Folder";
+            // 
+            // folderPath
+            // 
+            this.folderPath.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.folderPath.Location = new System.Drawing.Point(15, 25);
+            this.folderPath.Name = "folderPath";
+            this.folderPath.ReadOnly = true;
+            this.folderPath.Size = new System.Drawing.Size(356, 20);
+            this.folderPath.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.browseButton.Location = new System.Drawing.Point(377, 23);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "New pack format:";
+            // 
+            // packFormat
+            // 
+            this.packFormat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.packFormat.Location = new System.Drawing.Point(15, 76);
+            this.packFormat.Name = "packFormat";
+            this.packFormat.Size = new System.Drawing.Size(100, 20);
+            this.packFormat.TabIndex = 4;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.updateButton.Location = new System.Drawing.Point(121, 74);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 5;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // logText
+            // 
+            this.logText.AcceptsReturn = true;
+            this.logText.AcceptsTab = true;
+            this.logText.Font = new System.Drawing.Font("Consolas", 8.25F);
+            this.logText.Location = new System.Drawing.Point(15, 126);
+            this.logText.Multiline = true;
+            this.logText.Name = "logText";
+            this.logText.ReadOnly = true;
+            this.logText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.logText.Size = new System.Drawing.Size(437, 194);
+            this.logText.TabIndex = 7;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.clearButton.Location = new System.Drawing.Point(194, 326);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // formatCheck
+            // 
+            this.formatCheck.AutoSize = true;
+            this.formatCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.formatCheck.Location = new System.Drawing.Point(202, 80);
+            this.formatCheck.Name = "formatCheck";
+            this.formatCheck.Size = new System.Drawing.Size(125, 17);
+            this.formatCheck.TabIndex = 9;
+            this.formatCheck.Text = "Format pack.mcmeta";
+            this.formatCheck.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label3.Location = new System.Drawing.Point(424, 336);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "v1.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 361);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.formatCheck);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.logText);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.packFormat);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.folderPath);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Resource Pack Updater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox folderPath;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox packFormat;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.TextBox logText;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.CheckBox formatCheck;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ResourcePackUpdater/Form1.cs
@@ -0,0 +1,138 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResourcePackUpdater
+{
+    public partial class Form1 : Form
+    {
+        string path = "";
+        string[] subdirectories;
+
+        bool formatNull = true;
+        bool formatChecked;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+
+            DialogResult result = browserDialog.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                path = browserDialog.SelectedPath;
+                folderPath.Text = path;
+
+                subdirectories = Directory.GetDirectories(path);
+            }
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            UpdatePacks();
+        }
+
+        private void UpdatePacks()
+        {
+            try
+            {
+                if (path == "")
+                    logText.Text = "Error: Resource pack folder not selected.";
+                else if (packFormat.Text == "")
+                    logText.Text = "Error: Input is blank.";
+                else
+                {
+                    browseButton.Enabled = false;
+                    updateButton.Enabled = false;
+                    packFormat.Enabled = false;
+                    clearButton.Enabled = false;
+
+                    logText.Text = "";
+                    int packNumber = Int32.Parse(packFormat.Text);
+                    logText.Text += "Upgrading to pack format " + packNumber + "...\r\n\r\n";
+
+                    foreach (string str in subdirectories)
+                    {
+                        string filePath = str + "\\pack.mcmeta";
+                        string dirPath = Path.GetFileName(str);
+
+                        if (dirPath.Length > 40) dirPath = dirPath.Substring(0, 40);
+
+                        string status = "";
+
+                        if (File.Exists(filePath))
+                        {
+                            string json = File.ReadAllText(filePath);
+                            dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+
+                            try
+                            {
+                                if (!formatNull && formatChecked == formatCheck.Checked && jsonObj["pack"]["pack_format"] == packNumber)
+                                    status = "already updated";
+                                else
+                                {
+                                    if (jsonObj["pack"]["pack_format"] == packNumber)
+                                        status = "reformatted";
+                                    else
+                                        status = "completed";
+
+                                    jsonObj["pack"]["pack_format"] = packNumber;
+
+                                    string output = "";
+                                    if (formatCheck.Checked)
+                                        output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                                    else
+                                        output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj);
+
+                                    File.WriteAllText(filePath, output);
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                //Console.WriteLine(e.Message);
+                                status = "skipped";
+                            }
+                            
+                        }
+                        else
+                            status = "skipped";
+
+                        logText.Text += String.Format(
+                                    "{0,-40}{1,1}\r\n", dirPath, "\t... " + status);
+
+                        logText.Refresh();
+                    }
+                    formatChecked = formatCheck.Checked;
+                    formatNull = false;
+                    logText.Text += "\r\nDone.";
+                }
+            }
+            catch (FormatException)
+            {
+                logText.Text = "Error: Input is not a number.";
+            }
+
+            browseButton.Enabled = true;
+            updateButton.Enabled = true;
+            packFormat.Enabled = true;
+            clearButton.Enabled = true;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            logText.Text = "";
+        }
+    }
+}
